ff61e18245b20c6e93de8fc27ca7f0a2b897db64
Jeffrey Lym <jeffreyalym@gmail.com>
2018-09-09 13:39:16 -0700

    Basic changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import { remote, OpenDialogOptions } from 'electron';
 
+const openDialog = () => {
+  const dialog = remote.dialog;
+  const options: OpenDialogOptions = {};
+  dialog.showOpenDialog(remote.getCurrentWindow(), options);
+
+}
 export class App extends React.Component<undefined, undefined> {
   render() {
     return (
       <div>
-        <h2>Welcome to React with Typescript!</h2>
+        <h2>Welcome to React with Typescript!d</h2>
+
+        <button onClick={openDialog}>Sup</button>
       </div>
     );
   }
