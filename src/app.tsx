5bf2b735cae649f680ac7a5f946c0c29ee0aece4
Jeffrey Lym <jeffreyalym@gmail.com>
2018-09-09 18:22:28 -0700

    Get a basic form working

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,78 @@
 import * as React from 'react';
 import { remote, OpenDialogOptions } from 'electron';
+import { Button, FormControl } from 'react-bootstrap';
 
-const openDialog = () => {
-  const dialog = remote.dialog;
-  const options: OpenDialogOptions = {};
-  dialog.showOpenDialog(remote.getCurrentWindow(), options);
 
+interface IState {
+  path: string;
+  results: string;
 }
-export class App extends React.Component<undefined, undefined> {
-  render() {
-    return (
-      <div>
-        <h2>Welcome to React with Typescript!d</h2>
 
-        <button onClick={openDialog}>Sup</button>
+export class App extends React.Component<undefined, IState> {
+  public constructor(){
+    super();
+    this.state = {
+      path: '',
+      results: ''
+    };
+  }
+  public render() {
+    return (
+      <div style={
+        {
+          padding: 20,
+        }
+      }>
+        <h1>PBX File Processor</h1>
+
+        <div style={{
+          display: "grid",
+          gridTemplateColumns: "1fr auto",
+          gridGap: "10px"
+        }}>        
+          <FormControl
+            type="text"
+            placeholder="CSV File Path"
+            value={this.state.path}
+            />
+          <Button onClick={this.openDialog}>Open</Button>
+          <Button 
+            onClick={this.start}
+            style={{
+              gridColumn: "1 / -1",
+            }}>
+            Start
+          </Button>
+        </div>
+        
+        <p>
+          {this.state.results}
+        </p>
+        
+
+
       </div>
     );
   }
+
+  private openDialog = () => {
+    const dialog = remote.dialog;
+    const options: OpenDialogOptions = {};
+    dialog.showOpenDialog(remote.getCurrentWindow(), options, (filePaths: string[], _: string[]) => {
+      if (!filePaths || filePaths.length == 0) {
+        return;
+      }
+
+      this.setState({
+        path: filePaths[0]
+      });
+    });
+  }
+
+  private start = () => {
+    this.setState({
+      results: "ran"
+    });
+  }
+
 }
