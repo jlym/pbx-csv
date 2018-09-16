42b5c169d8f431dcf9cfb54a9e7b33b206f1e50e
Jeffrey Lym <jeffreyalym@gmail.com>
2018-09-15 23:09:05 -0700

    Implement logic to update csv

--- src/pbx.test.ts
+++ src/pbx.test.ts
@@ -1,6 +1,7 @@
+import { updatePBXFile } from './pbx';
 
 describe('updatePBXFile', () => {
     it('works', () => {
-
+        updatePBXFile('/home/jeffrey/go/src/github.com/jlym/pbx-csv-3/test_data/Master (4).csv', '/home/jeffrey/go/src/github.com/jlym/pbx-csv-3/test_data/out.csv', '/home/jeffrey/go/src/github.com/jlym/pbx-csv-3/test_data/PBX_Queue_000B82C04652');
     });
 })
--- src/pbx.ts
+++ src/pbx.ts
@@ -1,11 +1,68 @@
 import * as fs from 'fs';
-import * as path from 'path';
+import * as transform from 'stream-transform';
+import * as parse from 'csv-parse';
+import * as stringify from 'csv-stringify';
 
 export const updatePBXFile = (pbxInputFile: string, pbxOutputFile: string, mediaFileDirectory: string): void => {
 
+    const readStream = fs.createReadStream(pbxInputFile);
+    const out = fs.createWriteStream(pbxOutputFile);
+
+    let firstRow = true;
+    let uniqueIDIndex = -1;
+
+    const suffixToFile = getMediaFiles(mediaFileDirectory);
+    const tranformer = transform((record: any) => {
+        const cells = record as string[];
+        if (!cells) {
+            return record;
+        }
+        
+        if (firstRow) {
+            firstRow = false;
+            uniqueIDIndex = record.indexOf('uniqueid');
+            cells.push('link');
+            return cells;
+        }
+
+        if (uniqueIDIndex === -1) {
+            return cells;
+        }
+
+        const uniqueID = cells[uniqueIDIndex];
+        const uniqueIDTokens = uniqueID.split('.');
+        const uniqueIDSuffix = uniqueIDTokens[uniqueIDTokens.length - 1];
+
+        const file = suffixToFile.get(uniqueIDSuffix) || '';
+        cells.push(file);
+        
+        return cells;
+    });
+
+    
+    const parser = parse({delimiter: ',',});
+    readStream.pipe(parser).pipe(tranformer).pipe(stringify()).pipe(out);
 };
 
-const getMediaFiles = (mediaFileDirectory: string): string[] => {
-    return fs.readdirSync(mediaFileDirectory);
-};
+const getMediaFiles = (mediaFileDirectory: string): Map<string, string> => {
+    const files = fs.readdirSync(mediaFileDirectory);
 
+    const suffixToFile = new Map<string, string>();
+    
+    for (let file of files) {
+        file = file.toLowerCase();
+        if (!file.endsWith('.wav')) {
+            continue;
+        }
+
+        const tokens = file.split('.');
+        if (tokens.length < 3) {
+            continue;
+        }
+
+        const suffix = tokens[tokens.length - 2];
+        suffixToFile.set(suffix, mediaFileDirectory + '/' + file);
+    }
+
+    return suffixToFile;
+};
